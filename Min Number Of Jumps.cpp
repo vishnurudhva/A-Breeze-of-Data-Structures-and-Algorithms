cb6622152d34d9c309ba00eab520d8363d9eb148
Vishnu Rudhva <vishnurudhva01@gmail.com>
2020-08-27 02:38:37 +0530

    Create Min Number Of Jumps.cpp

---
+++ Min Number Of Jumps.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+using namespace std;
+
+/*
+You're given a non-empty array of positive integers where each integer represents the maximum number of steps you can take forward in the array. For example, if the element at index 1 is 3, you can go from index 1 to index 2, 3, or 4.
+
+Write a function that returns the minimum number of jumps needed to reach the final index.
+
+Note that jumping from index i to index i + x always constitutes one jump, no matter how large x is.
+
+Sample Input
+array = [3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3]
+Sample Output
+4 // 3 --> (4 or 2) --> (2 or 3) --> 7 --> 3
+*/
+
+int minNumberOfJumps(vector<int> array) {
+	if(array.size() == 1) return 0;
+	int n = array.size(), jumps = 0;
+	int maxReach = array[0], steps = array[0];
+  for(int i = 1; i < n - 1; i++){
+		maxReach = max(maxReach, i + array[i]);
+		steps--;
+		if(steps == 0){
+			jumps++;
+			steps = maxReach - i;
+		}
+	}
+  return jumps + 1;
+}
